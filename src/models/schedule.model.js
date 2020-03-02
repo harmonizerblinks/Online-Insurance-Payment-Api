dbb4763b9bd3fa6b53477120b77675a0c18af40b
dari <mdari61@gmail.com>
2020-03-02 20:58:24 +0000

    Saving changes

--- src/models/schedule.model.js
+++ src/models/schedule.model.js
@@ -4,7 +4,7 @@ const uniqueValidator = require('mongoose-unique-validator');
 
 const ScheduleSchema = mongoose.Schema({
     code: { type: String, index: true, required: true, unique: true },
-    // name: { type: String, index: true, required: true },
+    type: { type: String, index: true, required: true },
     loc: { type: [String], required: false },
     start_point: { type: Schema.Types.ObjectId, ref: 'station', required: false },
     end_point: { type: Schema.Types.ObjectId, ref: 'station', required: false },
@@ -18,7 +18,7 @@ const ScheduleSchema = mongoose.Schema({
     notice: { type: String, required: false },
     status: { type: String, required: true, default: 'Upcoming' },
     haspickup: { type: Boolean, required: true },
-    stations: { type: [Schema.Types.ObjectId], ref: 'station', required: false, default: '' },
+    stations: { type: [Schema.Types.ObjectId], ref: 'station', required: false },
     driverid: { type: String, required: false },
     busid: { type: Schema.Types.ObjectId, ref: 'bus', required: false },
     locationid: { type: Schema.Types.ObjectId, ref: 'location', required: false },
