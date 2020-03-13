89beabe2f5de6dc5c50b7b3dfe1f1a1ba5901ed3
dari <mdari61@gmail.com>
2020-03-13 12:21:18 +0000

    Changing database

--- src/config/mongodb.config.js
+++ src/config/mongodb.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    url: 'mongodb+srv://Acyst:Developer@12g@cluster0-xc6xi.azure.mongodb.net/test?retryWrites=true&w=majority',
+    url: 'mongodb+srv://Acyst:Developer@12g@cluster0-xc6xi.azure.mongodb.net/Troski?retryWrites=true&w=majority',
     url1: 'mongodb://127.0.0.1:27017/Fbtwm-xmas?retryWrites=true&w=majority',
     app: 'http://localhost:5000/public/',
     secret: '1234567890abcdefghijklmnopqrstuvwxyz',
